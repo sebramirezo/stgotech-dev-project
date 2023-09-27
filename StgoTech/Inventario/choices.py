7049c53e740bc0e66af111b767d143f190115854
Sebastián Ramírez <seb.ramirezo@duocuc.cl>
2023-09-26 21:07:39 -0300

    actualizacion models

---
+++ StgoTech/Inventario/choices.py
@@ -0,0 +1,65 @@
+clasificacion_consu = [
+    ('None', 'N/A'),
+    ('COMBUSTIBLE', 'COMBUSTIBLE'),
+    ('CONSUMOS', 'CONSUMOS'),
+    ('HERRAMIENTA', 'HERRAMIENTA'),
+    ('INSUMOS', 'INSUMOS'),
+    ('ROTABLES', 'ROTABLES'),
+    ('TOOL', 'TOOL'),
+    ('TOOL-CAL', 'TOOL-CAL')
+]
+condition = [
+    ('None', 'N/A'),
+    ('SVC', 'SVC'),
+    ('QUA', 'QUA'),
+    ('NSV', 'NSV')
+
+]
+
+n_ficha = [
+    ('None', 'N/A'),
+    ('1,1', '1,1'),
+    ('1,2', '1,2'),
+    ('1,3', '1,3'),
+    ('2,1', '2,1'),
+    ('2,2', '2,2'),
+    ('2,3', '2,3'),
+    ('3,1', '3,1'),
+    ('3,2', '3,2'),
+    ('3,3', '3,3')
+
+]
+
+owner = [
+    ('None', 'N/A'),
+    ('FHS', 'FHS'),
+    ('JAT', 'JAT'),
+    ('IMS', 'IMS'),
+    ('STGO', 'STGO')
+
+]
+
+
+u_oM = [
+    ('None', 'N/A'),
+    ('CO', 'CO'),
+    ('E/A', 'E/A'),
+    ('FT', 'FT'),
+    ('GAL', 'GAL'),
+    ('GL', 'GL'),
+    ('GR', 'GR'),
+    ('IN', 'IN'),
+    ('KT', 'KT'),
+    ('M2', 'M2'),
+    ('MT', 'MT'),
+    ('PL', 'PL'),
+    ('QT', 'QT'),
+    ('TB', 'TB')
+
+]
+
+ABONA_CANCELA = [
+    ('None', 'N/A'),
+    ('ABONA', 'ABONA'),
+    ('CANCELA', 'CANCELA')
+]
