a8b3a3c50e8d7bc718bf36fc25827a3cfb0fc9c0
Sebastián Ramírez <seb.ramirezo@duocuc.cl>
2023-11-17 16:33:42 -0300

    update config

--- StgoTech/StgoTech/deployment.py
+++ StgoTech/StgoTech/deployment.py
@@ -43,9 +43,9 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': conn_str_params['inventario-stgotech-database'],
+        'HOST': conn_str_params['inventario-stgotech-server'],
+        'USER': conn_str_params['cdqmsgmfoq'],
+        'PASSWORD': conn_str_params['NMPII0S03W88Z48I$'],
     }
 }
